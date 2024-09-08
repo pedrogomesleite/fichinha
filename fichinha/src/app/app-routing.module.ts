d39134025ada1da865ebe483d699a2f8b90c0e9e
pedrogomesleite <pedro.gomes.leite@gmail.com>
2024-09-08 20:27:11 -0300

    segundo commit

--- fichinha/src/app/app-routing.module.ts
+++ fichinha/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {InicioComponent} from "./inicio/componentes/inicio/inicio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent, title: 'Inicio'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
